7050ffc1bfc162fcf272f48808dec596374b6a13
ChiekoTamura <202405ap01_tamura@x-ide.sakura.ne.jp>
2024-08-02 11:50:14 +0900

    #25752　ファイルを更新する際、dfファイル以外をアップロードできてしまう
    DBに既存ファイルの有無に関わらず、拡張子がpdfかどうかをチェックするよう修正しました

--- SRC/js/fmanager.js
+++ SRC/js/fmanager.js
@@ -51,10 +51,11 @@ function fnFManagerViewEditCheck() {
 	}
 
 	tmp = form.pdfFile.value;
-	if (!form.pdfNo.value && tmp.slice(-4) != '.pdf' && tmp.slice(-4) != '.PDF') {
+	if (tmp.slice(-4) != '.pdf' && tmp.slice(-4) != '.PDF') {
 		alert('PDFファイルを指定してください');
 		return;
 	}
+
 
 	if (confirm('この内容で登録します。よろしいですか？')) {
 		form.act.value = 'fManagerViewEditComplete';
